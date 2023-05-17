2b0ce0746fdc3bf21d0e9f40a9de08dfeaed5319
ferdys30 <ferdyseptiawan02@gmail.com>
2023-05-18 06:50:19 +0700

    function upload

--- db/function.php
+++ db/function.php
@@ -148,7 +148,7 @@ function upload()
     $namafilebaru .= $ekstensigambar;
 
     // mengirim kedalam directory 
-    move_uploaded_file($tmpname, 'D:/Aplikasi/xampp/htdocs/DigiSkill/Assets/profile/' . $namafilebaru);
+    move_uploaded_file($tmpname, '../../Assets/profile/' . $namafilebaru);
 
     return $namafilebaru; // untuk diproses dalam function tambah sebagai nama file yang disimpan
 
@@ -201,10 +201,116 @@ function uploadasst()
     $namafilebaru .= $ekstensigambar;
 
     // mengirim kedalam directory 
-    move_uploaded_file($tmpname, 'D:/Aplikasi/xampp/htdocs/DigiSkill/Assets/' . $namafilebaru);
+    move_uploaded_file($tmpname, '../../Assets/' . $namafilebaru);
 
     return $namafilebaru; // untuk diproses dalam function tambah sebagai nama file yang disimpan
 }
+
+////////////////function untuk upload gambar profile yang disimpan ke dalam profile
+function editupload()
+{
+    $namafile = $_FILES['foto']['name'];
+    $ukuranfile = $_FILES['foto']['size'];
+    $error = $_FILES['foto']['error'];
+    $tmpname = $_FILES['foto']['tmp_name'];
+
+    // cek apakah ada gambar yang diupload
+    if ($error === 4) {
+        echo "<script>
+        alert ('Pilih Gambar terlebih dahulu');
+        </script>";
+        return false;
+    }
+
+    // cek yang diupload ekstensinya apkah gambar atau bukan
+    $ekstensigambarValid = ['jpg', 'png', 'jpeg'];
+    $ekstensigambar = explode('.', $namafile); // sebagai pemisah antara nama file dengan ekstensi
+    //  contohnya ferdy.png maka menjadi ['ferdy','png']
+    $ekstensigambar = strtolower(end($ekstensigambar)); // strtolower merubah nama file menjadi lowercase/ huruf kecil
+    // end mengambil explode yang paling akhir
+
+    if (!in_array($ekstensigambar, $ekstensigambarValid)) { // melakukan pengecekan string didalam array
+        // fungsi ini menghasilkan true jika ada false jika tidak
+        echo "<script>
+        alert ('Yang anda upload bukan gambar');
+        </script>";
+        return false;
+    }
+
+    // cek jika ukurannya terlalu besar
+    if ($ukuranfile > 5000000000) { //dalam bentuk byte
+        echo "<script>
+        alert ('file yang anda upload terlalu besar');
+        </script>";
+        return false;
+    }
+
+    //lolos pengecekan gambar siap diupload
+
+    // generate nama gambar random baruu
+    $namafilebaru = uniqid();
+    $namafilebaru .= '.';
+    $namafilebaru .= $ekstensigambar;
+
+    // mengirim kedalam directory 
+    move_uploaded_file($tmpname, '../../../Assets/profile/' . $namafilebaru);
+
+    return $namafilebaru; // untuk diproses dalam function tambah sebagai nama file yang disimpan
+
+}
+
+////////////////function untuk upload gambar asset dalam program 
+function edituploadasst()
+{
+    $namafile = $_FILES['foto']['name'];
+    $ukuranfile = $_FILES['foto']['size'];
+    $error = $_FILES['foto']['error'];
+    $tmpname = $_FILES['foto']['tmp_name'];
+
+    // cek apakah ada gambar yang diupload
+    if ($error === 4) {
+        echo "<script>
+        alert ('Pilih Gambar terlebih dahulu');
+        </script>";
+        return false;
+    }
+
+    // cek yang diupload ekstensinya apkah gambar atau bukan
+    $ekstensigambarValid = ['jpg', 'png', 'jpeg'];
+    $ekstensigambar = explode('.', $namafile); // sebagai pemisah antara nama file dengan ekstensi
+    //  contohnya ferdy.png maka menjadi ['ferdy','png']
+    $ekstensigambar = strtolower(end($ekstensigambar)); // strtolower merubah nama file menjadi lowercase/ huruf kecil
+    // end mengambil explode yang paling akhir
+
+    if (!in_array($ekstensigambar, $ekstensigambarValid)) { // melakukan pengecekan string didalam array
+        // fungsi ini menghasilkan true jika ada false jika tidak
+        echo "<script>
+        alert ('Yang anda upload bukan gambar');
+        </script>";
+        return false;
+    }
+
+    // cek jika ukurannya terlalu besar
+    if ($ukuranfile > 5000000000) { //dalam bentuk byte
+        echo "<script>
+        alert ('file yang anda upload terlalu besar');
+        </script>";
+        return false;
+    }
+
+    //lolos pengecekan gambar siap diupload
+
+    // generate nama gambar random baruu
+    $namafilebaru = uniqid();
+    $namafilebaru .= '.';
+    $namafilebaru .= $ekstensigambar;
+
+    // mengirim kedalam directory 
+    move_uploaded_file($tmpname, '../../../Assets/' . $namafilebaru);
+
+    return $namafilebaru; // untuk diproses dalam function tambah sebagai nama file yang disimpan
+}
+
 ///////////////// Function masuk kelas
 function mskkelas($data)
 {
@@ -260,7 +366,7 @@ function editkelas($data)
     if ($_FILES['foto']['error'] === 4) {
         $thumbnail = $fotolama;
     } else {
-        $thumbnail = uploadasst();
+        $thumbnail = edituploadasst();
     }
 
     $query = "UPDATE kelas SET
@@ -323,7 +429,7 @@ function editmentor($data)
     if ($_FILES['foto']['error'] === 4) {
         $foto = $fotolama;
     } else {
-        $foto = uploadasst();
+        $foto = edituploadasst();
     }
 
     $query = "UPDATE mentor SET
@@ -383,7 +489,7 @@ function edittools($data)
     if ($_FILES['foto']['error'] === 4) {
         $foto = $fotolama;
     } else {
-        $foto = uploadasst();
+        $foto = edituploadasst();
     }
 
     $query = "UPDATE tools SET
