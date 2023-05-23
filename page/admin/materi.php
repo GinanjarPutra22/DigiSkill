4624d807645b3b3a8dbcd5cf7992fe80d42ed724
Ginanjar Putra <64942734+GinanjarPutra22@users.noreply.github.com>
2023-05-23 21:09:07 +0700

    edit username in navbar admin

--- page/admin/materi.php
+++ page/admin/materi.php
@@ -30,7 +30,8 @@ if (isset($_POST["cari"])) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Mentors</title>
   <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
   <!-- Icon Title -->
   <link rel="icon" href="../Assets/logo-icon.svg" type="image/x-icon" />
@@ -47,10 +48,12 @@ if (isset($_POST["cari"])) {
   <nav class="navbar navbar-expand-lg bg-light shadow-sm bg-body rounded">
     <div class="container">
       <a class="navbar-brand" href="../../index.php">
-        <img src="../../Assets/Logo-DigiSkill.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+        <img src="../../Assets/Logo-DigiSkill.svg" alt="Logo" width="30" height="24"
+          class="d-inline-block align-text-top">
         DigiSkill
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
@@ -64,8 +67,8 @@ if (isset($_POST["cari"])) {
             </a>
             <ul class="dropdown-menu me-4">
               <?php
-              foreach ($kelas as $row) :
-              ?>
+              foreach ($kelas as $row):
+                ?>
                 <li>
                   <a class="dropdown-item" href="../detail/detail.php?id=<?= $row['id_kelas'] ?>"><?= $row['nama_kelas'] ?></a>
                 </li>
@@ -81,8 +84,16 @@ if (isset($_POST["cari"])) {
           <?php
           if (isset($_SESSION["login"])) { ?>
             <li class="nav-item dropdown me-4">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="../../Assets/profile/<?= $profile['foto'] ?>" class="rounded-circle" height="22" alt="Foto" loading="lazy" />
+              <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                <div class="account d-flex">
+                  <img src="../../Assets/profile/<?= $profile['foto'] ?>" class="rounded-circle me-3" height="32"
+                    alt="Pict" loading="lazy" />
+                  <p class="dropdown-toggle">
+                    <?= $_SESSION["nama"] ?>
+                  </p>
+                </div>
+
               </a>
 
               <ul class="dropdown-menu me-4">
@@ -145,7 +156,8 @@ if (isset($_POST["cari"])) {
                 </a>
 
                 <a href="materi.php" class="d-grid gap-2">
-                  <button class="btn btn-primary disabled" type="button"> <span><i class='bx bx-book-bookmark'></i></span>
+                  <button class="btn btn-primary disabled" type="button"> <span><i
+                        class='bx bx-book-bookmark'></i></span>
                     Materi</button>
                 </a>
               </div>
@@ -172,15 +184,17 @@ if (isset($_POST["cari"])) {
                 <div class="col-lg-7 d-flex">
                   <select class="form-select 3 me-lg-2 me-2" aria-label="Default select example" name="kelas">
                     <option selected value="">Pilih Kelas</option>
-                    <?php foreach ($kelas as $row) : ?>
+                    <?php foreach ($kelas as $row): ?>
                       <option value="<?= $row['id_kelas'] ?>"><?= $row['nama_kelas'] ?></option>
                     <?php endforeach ?>
                   </select>
 
-                  <button class="btn btn-primary btn-md d-flex" type="submit" name="cari"> <span class="me-2"><i class='bx bx-filter'></i></span>
+                  <button class="btn btn-primary btn-md d-flex" type="submit" name="cari"> <span class="me-2"><i
+                        class='bx bx-filter'></i></span>
                     Filter</button>
                   <!-- Btn refresh -->
-                  <button class="btn btn-primary btn-md d-flex ms-2"> <span class=""><i class='bx bx-refresh'></i></span>
+                  <button class="btn btn-primary btn-md d-flex ms-2"> <span class=""><i
+                        class='bx bx-refresh'></i></span>
                 </div>
 
                 <div class="col-lg-5 mt-3 mt-lg-0">
@@ -207,7 +221,7 @@ if (isset($_POST["cari"])) {
                 <th class="text-center">Tindakan</th>
 
               </tr>
-              <?php foreach ($materi as $row) : ?>
+              <?php foreach ($materi as $row): ?>
                 <tr>
                   <td class="text-center">
                     <?= $row["urutan"] ?>
@@ -225,8 +239,11 @@ if (isset($_POST["cari"])) {
                   </td>
                   <td class="text-center">
                     <div class="d-flex">
-                      <a href="edit/edit-materi.php?id=<?= $row["id_materi"] ?>" type="button" class="btn btn-primary btn-sm mx-1">Edit</a>
-                      <a href="hapus/hapus-materi.php?id=<?= $row["id_materi"] ?>" type="button" class="btn btn-danger btn-sm mx-1" onclick="return confirm('Apakah Anda Yakin Menghapus data ini?')">Hapus</a>
+                      <a href="edit/edit-materi.php?id=<?= $row["id_materi"] ?>" type="button"
+                        class="btn btn-primary btn-sm mx-1">Edit</a>
+                      <a href="hapus/hapus-materi.php?id=<?= $row["id_materi"] ?>" type="button"
+                        class="btn btn-danger btn-sm mx-1"
+                        onclick="return confirm('Apakah Anda Yakin Menghapus data ini?')">Hapus</a>
 
                     </div>
                   </td>
@@ -248,7 +265,9 @@ if (isset($_POST["cari"])) {
   <!-- ------End Row Main Detail Kelas------- -->
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
