fe0f157e09af06bef936961e7f6cd710cce48cc1
Ginanjar Putra <64942734+GinanjarPutra22@users.noreply.github.com>
2023-06-06 01:05:00 +0700

    edi

--- page/login.php
+++ page/login.php
@@ -56,7 +56,7 @@ if (isset($_POST["masuk"])) {
   <!-- <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
         rel="stylesheet"> -->
   <!-- My CSS -->
-  <link rel="stylesheet" href="/css/login-regis.css" />
+  <link rel="stylesheet" href="../css/login-regis.css" />
 </head>
 
 <body>
